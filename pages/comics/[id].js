646b4a2eb9869c830dd81a0d631a9e43aaf933d4
Iris Leal <iris_leal_m@hotmail.com>
2020-07-02 19:41:28 -0700

    Now renders the title of the selected element

--- pages/comics/[id].js
+++ pages/comics/[id].js
@@ -1,10 +1,17 @@
 export default function ComicDetail(props){
-    return <h3>Im one comic  {props.comics.safe_title} </h3>
+    // return <h3>Im one comic  {props.comics.safe_title} </h3>
+    return (
+        <>
+          <h3>Im one comic </h3>    
+          <label> {props.comics.safe_title} </label>
+        </>
+      )
 }
 
 export async function getServerSideProps(context){
-  // https://xkcd.com/2326/info.0.json
-  const response = await fetch(`http://localhost:3000/api/comics/${context.params.id}`);
+  // const response = await fetch(`http://localhost:3000/api/comics/${context.params.id}`);
+  const url = `https://xkcd.com/${context.params.id}/info.0.json`
+  const response = await fetch(url);
   const comics = await response.json();
 
   return {
